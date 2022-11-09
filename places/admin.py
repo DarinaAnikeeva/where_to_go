81d2f0f3bf9055ffd97bd07262b99f37b1ef03a0
DarinaAnikeeva <102724828+DarinaAnikeeva@users.noreply.github.com>
2022-11-09 10:33:16 +0300

    add  adminsortable2

--- places/admin.py
+++ places/admin.py
@@ -1,14 +1,29 @@
 from django.contrib import admin
+from adminsortable2.admin import SortableAdminMixin, SortableTabularInline
+from django.utils.html import format_html
 from places.models import *
 
-class ImageInline(admin.TabularInline):
-    model = Image
+
+class ImageInline(SortableTabularInline):
+  model = Image
+  readonly_fields = ["preview"]
+  fields = ('img', 'preview', 'number_img')
+
+  def preview(self, obj):
+      return format_html(f'<img src="{obj.img.url}" style="max-height: 100px;">')
+
 
 @admin.register(Place)
-class PlaceAdmin(admin.ModelAdmin):
+class PlaceAdmin(SortableAdminMixin, admin.ModelAdmin):
     inlines = [
       ImageInline
     ]
 
-admin.site.register(Image)
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+  fields = ('place', 'img', 'number_img', 'preview')
+  readonly_fields = ["preview"]
+
+  def preview(self, obj):
+      return format_html(f'<img src="{obj.img.url}" style="max-height: 100px;">')
 # Register your models here.
