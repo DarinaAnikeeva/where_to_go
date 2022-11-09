2a55b61a18a23d64023605ee7d874f1646c8d53c
DarinaAnikeeva <102724828+DarinaAnikeeva@users.noreply.github.com>
2022-11-09 10:32:25 +0300

    add class Meta

--- places/models.py
+++ places/models.py
@@ -7,14 +7,21 @@ class Place(models.Model):
   lng = models.FloatField(null=False)
   lat = models.FloatField(null=False)
 
+  class Meta:
+    ordering = ['id']
+
   def __str__(self):
     return self.title
+
 
 
 class Image(models.Model):
   place = models.ForeignKey(Place, on_delete=models.CASCADE, related_name="images", blank=True, null=True)
   img = models.ImageField(blank=False)
   number_img = models.IntegerField(default=1, blank=True)
+
+  class Meta:
+    ordering = ['number_img']
 
   def __str__(self):
     return f'{self.number_img} {self.place}'
