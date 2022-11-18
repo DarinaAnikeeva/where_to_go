4b42907ca5df64b89bb86c2812dbd830f3c40ae7
DarinaAnikeeva <102724828+DarinaAnikeeva@users.noreply.github.com>
2022-11-18 21:12:10 +0300

    Update models.py

--- places/models.py
+++ places/models.py
@@ -1,64 +1,64 @@
 from django.db import models
 from tinymce.models import HTMLField
 
+
 class Place(models.Model):
-  title = models.CharField(
-        verbose_name="Название места",
-        max_length=200,
-        blank=False
-  )
+    title = models.CharField(
+      verbose_name="Название места",
+      max_length=200,
+      blank=False
+    )
 
-  description_short = models.TextField(
-        verbose_name="Короткое описание",
-        blank=True
-  )
+    description_short = models.TextField(
+      verbose_name="Короткое описание",
+      blank=True
+    )
 
-  description_long = HTMLField(
-        verbose_name="Длинное описание",
-        blank=True
-  )
+    description_long = HTMLField(
+      verbose_name="Длинное описание",
+      blank=True
+    )
 
-  lng = models.FloatField(
-        verbose_name="Широта",
-        null=False
-  )
+    lng = models.FloatField(
+      verbose_name="Широта",
+      null=False
+    )
 
-  lat = models.FloatField(
-        verbose_name="Долгота",
-        null=False
-  )
+    lat = models.FloatField(
+      verbose_name="Долгота",
+      null=False
+    )
 
-  class Meta:
-    ordering = ['id']
+    class Meta:
+        ordering = ['id']
 
-  def __str__(self):
-    return self.title
-
+    def __str__(self):
+        return self.title
 
 
 class Image(models.Model):
-  place = models.ForeignKey(
-        Place,
-        verbose_name="Название места",
-        on_delete=models.CASCADE,
-        related_name="images",
-        blank=True,
-        null=True
-  )
+    place = models.ForeignKey(
+      Place,
+      verbose_name="Название места",
+      on_delete=models.CASCADE,
+      related_name="images",
+      blank=True,
+      null=True
+    )
 
-  img = models.ImageField(
-        verbose_name="Изображение",
-        blank=False
-  )
+    img = models.ImageField(
+      verbose_name="Изображение",
+      blank=True
+    )
 
-  number_img = models.IntegerField(
-        verbose_name="Номер изображения",
-        default=1,
-        blank=True
-  )
+    number_img = models.IntegerField(
+      verbose_name="Номер изображения",
+      default=1,
+      blank=True
+    )
 
-  class Meta:
-    ordering = ['number_img']
+    class Meta:
+        ordering = ['number_img']
 
-  def __str__(self):
-    return f'{self.number_img} {self.place}'
+    def __str__(self):
+        return self.number_img, self.place
