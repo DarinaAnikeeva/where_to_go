b3f19307ff1f5058da99e09ab7dfd79f16e63fcb
DarinaAnikeeva <102724828+DarinaAnikeeva@users.noreply.github.com>
2022-11-18 21:13:49 +0300

    replace preview to show_photo

--- places/admin.py
+++ places/admin.py
@@ -1,29 +1,26 @@
+from adminsortable2.admin import SortableAdminBase, SortableTabularInline
 from django.contrib import admin
-from adminsortable2.admin import SortableAdminMixin, SortableTabularInline
-from django.utils.html import format_html
-from places.models import *
+
+from .admin_function import show_photo
+from places.models import Place, Image
 
 
 class ImageInline(SortableTabularInline):
-  model = Image
-  readonly_fields = ["preview"]
-  fields = ('img', 'preview', 'number_img')
-
-  def preview(self, obj):
-      return format_html(f'<img src="{obj.img.url}" style="max-height: 100px;">')
+    model = Image
+    readonly_fields = ["show_photo"]
+    fields = ('img', 'show_photo', 'number_img')
+    show_photo = show_photo
 
 
 @admin.register(Place)
-class PlaceAdmin(SortableAdminMixin, admin.ModelAdmin):
+class PlaceAdmin(SortableAdminBase, admin.ModelAdmin):
     inlines = [
       ImageInline
     ]
 
+
 @admin.register(Image)
 class ImageAdmin(admin.ModelAdmin):
-  fields = ('place', 'img', 'number_img', 'preview')
-  readonly_fields = ["preview"]
-
-  def preview(self, obj):
-      return format_html(f'<img src="{obj.img.url}" style="max-height: 100px;">')
-
+    fields = ('place', 'img', 'number_img', 'show_photo')
+    readonly_fields = ["show_photo"]
+    show_photo = show_photo
